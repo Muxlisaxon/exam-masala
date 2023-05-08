73f17dd2e3ae0f0ae31387ddb7fff10bea37cc24
Muxlisaxon <qochqorovamuxlisam@gmail.com>
2023-05-08 20:22:13 +0500

    hhhh

---
+++ index.js
@@ -0,0 +1,44 @@
+// 1. Write a function that takes an array of numbers as input and returns the sum of all the even numbers in the array.
+// sumOfEvens([1, 2, 3, 4, 5, 6]) => 12 juft sonlar yig'indisi
+// 2.Write a function that takes an array of numbers as input and returns the largest number in the array.
+// flargestNumber( [10, 5, 20, 8, 15]) =>  20 eng kattasini
+// 3. Write a function that takes two strings as input and returns true if the second string is a substring of the first string, and false otherwise.     
+// let string1 = "hello world";
+// let string2 = "wor";
+// isSubstring(string1, string2) => true
+// 4. Create an object that contains information about a person, including their name, age, and email address. Write a function that takes this object as input and returns the person"s name and email address formatted as "Name: email@example.com".
+// formatPerson(person) // Output: John Doe: john.doe@example.com
+// 5. Write a function that takes an array of strings as input and returns a new array containing only the strings that have at least one uppercase letter.
+// let strings = ["hello", "WORLD", "foo", "bar", "BAZ"];
+// console.log(uppercaseStrings(strings)); // Output: ["WORLD", "BAZ"]
+// https://ijazulrehman.github.io/Minimalistic-Startup-Landing-Page/video-demo
+
+
+
+1) function hisobla(i) {
+    for (let i = 0; i < 10; i = i + 2) {
+        return i++
+    }
+}
+
+
+2) var arr = [10, 5, 20, 8, 15]
+console.log(arr.Math.max)
+
+
+
+
+4) function gmail(name) {
+    var x = name
+    return x + "email@example.com"
+}
+
+3)function ggg (str1, str2){
+    let str1 = "hello world";
+    let str2 = "wor";
+if( str1 == str2){
+    return true
+}else{
+    return false
+}
+}
